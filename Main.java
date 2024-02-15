2047392460709a20ab815747ffe9c3a7d22a3169
Andrea Caitano Baron <andrea.caitano.baron@avanade.com>
2024-02-15 12:40:20 -0300

    enunciado do exercicios

--- Main.java
+++ Main.java
@@ -6,6 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// Problema exemplo:
+
+		// Fazer um programa para ler um valor inteiro de 1 a 7 representado
+		// um dia da semana (sendo 1=domingo, 2=segundqa e assim por diante).
+		// Escreva na tela o dia da semana corespondente, conforme exemplos.
+
+		// entreda: 1, Saida Domingo
+		// entrada 2, Saida Segundo
+		// entrada 9 Dia da semana: nválido digite um número de um a 7
+
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Que dia da semana e hj digite número");
@@ -39,7 +49,7 @@ public class Main {
 			dia = "inválido digite um número de um a 7";
 
 		}
-		System.out.println("Dia da semana " + dia);
+		System.out.println("Dia da semana: " + dia);
 		sc.close();
 
 	}
